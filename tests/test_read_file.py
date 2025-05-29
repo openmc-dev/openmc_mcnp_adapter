be32a655d52a9907e23b1820a0b8384358d8eae2
Travis J. Labossiere-Hickman <Travis.LabossiereHickman@INL.gov>
2025-05-29 10:08:13 -0600

    Test 'read file=' card against ref solution

--- tests/test_read_file.py
+++ tests/test_read_file.py
@@ -1,7 +1,12 @@
+import os
 import textwrap
 import pytest
 
 from openmc_mcnp_adapter import mcnp_str_to_model
+from openmc_mcnp_adapter.parse import read_file
+
+here = os.path.dirname(os.path.abspath(__file__))
+input_dir = os.path.join(here, "inputs")
 
 
 def test_read_not_found():
@@ -12,3 +17,9 @@ def test_read_not_found():
 	""")
 	with pytest.raises(FileNotFoundError):
 		mcnp_str_to_model(deck)
+
+
+def test_read_recursive():
+	reference = read_file(os.path.join(input_dir, "testReadReference.imcnp"))
+	trial = read_file(os.path.join(input_dir, "testRead.imcnp"))
+	assert trial == reference
